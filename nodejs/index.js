d67a8a2e02eb2bf18538c4d4240397b74b84f82d
Alexander Rumyankov <alex@brain4net.com>
2021-05-19 00:21:05 +0300

    add NodeJs code

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const https = require('https');
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 const wav = require('wav');
@@ -6,18 +7,79 @@ const wav = require('wav');
 const username = process.env.SBERSPEECH_USERNAME;
 const password = process.env.SBERSPEECH_PASSWORD;
 
-var file = fs.createReadStream(process.argv[2]);
+var audio = fs.createReadStream(process.argv[2]);
 var reader = new wav.Reader();
 
-const request = {
-    options: {
-        audio_encoding: 1, // PCM_S16LE
-        sample_rate: 16000,
-        language: 'ru-RU',
-        hypotheses_count: 1,
-        enable_profanity_filter: true,
-        enable_multi_utterance: false,
-        enable_partial_results: true,
-        model: 'general'
-    }
+const CHUNK_SIZE = 4000;
+const FREQUENCY = 250;
+
+const packageDefinition = protoLoader.loadSync('./proto/asr.proto', {
+    includeDirs: ['node_modules/google-proto-files', './proto']
+});
+const packageObject = grpc.loadPackageDefinition(packageDefinition);
+const serviceConstructor = packageObject.smartspeech.recognition.v1.SmartSpeech;
+
+const grpcRequestConfig = {
+  options: {
+    audio_encoding: packageObject.smartspeech.recognition.v1.RecognitionOptions.PCM_S16LE,
+    sample_rate: 16000,
+    language: 'ru-RU',
+    hypotheses_count: 1,
+    enable_profanity_filter: true,
+    enable_multi_utterance: false,
+    enable_partial_results: true,
+    model: 'general'
+  }
 };
+
+var jwtRequestOptions = {
+  host: 'smartspeech.sber.ru',
+  port: 443,
+  path: '/v1/token',
+  method: 'POST',
+  headers: {
+    'Authorization': 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
+  }   
+};
+
+var jwtRequest = https.get(jwtRequestOptions, function(jwtResponse) {
+  var jwtResponseBody = "";
+  jwtResponse.on('data', function(jwtResponseData) {
+      jwtResponseBody += jwtResponseData;
+  });
+  jwtResponse.on('end', function() {
+    var jwt = JSON.parse(jwtResponseBody).tok;
+    console.log(`Token: ${jwt}`);
+    var serviceMetadata = new grpc.Metadata();
+    serviceMetadata.add('authorization', `Bearer ${jwt}`);
+    var grpcCredentials = grpc.credentials.createSsl(fs.readFileSync('./roots.pem'));
+    var service = new serviceConstructor('smartspeech.sber.ru:443', grpcCredentials);
+    var call = service['Recognize'](serviceMetadata);
+    call.write(grpcRequestConfig);
+
+    let i = 1;
+    const interval = setInterval(() => {
+      if (i * CHUNK_SIZE <= audio.length) {
+        const chunk = new Uint16Array(audio.slice((i - 1) * CHUNK_SIZE, i * CHUNK_SIZE));
+        const chunkBuffer = Buffer.from(chunk);
+        call.write({audioContent: chunkBuffer});
+        i++;
+      } else {
+        call.end();
+        clearInterval(interval);
+      }
+    }, FREQUENCY);
+    
+    call.on('data', (response) => {
+      console.log(response);
+    });
+
+    call.on('error', (response) => {
+      console.log(response);
+    });
+  })
+  jwtResponse.on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+});
+
