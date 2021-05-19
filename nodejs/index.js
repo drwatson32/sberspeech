bb28d6e049ceb06ae083a47965f937dacac78007
Alexander Rumyankov <alex@brain4net.com>
2021-05-19 12:23:29 +0300

    fix api

--- nodejs/index.js
+++ nodejs/index.js
@@ -19,17 +19,15 @@ const packageDefinition = protoLoader.loadSync('./proto/asr.proto', {
 const packageObject = grpc.loadPackageDefinition(packageDefinition);
 const serviceConstructor = packageObject.smartspeech.recognition.v1.SmartSpeech;
 
-const grpcRequestConfig = {
-  options: {
-    audio_encoding: packageObject.smartspeech.recognition.v1.RecognitionOptions.PCM_S16LE,
-    sample_rate: 16000,
-    language: 'ru-RU',
-    hypotheses_count: 1,
-    enable_profanity_filter: true,
-    enable_multi_utterance: false,
-    enable_partial_results: true,
-    model: 'general'
-  }
+const recognitionOptions = {
+  audio_encoding: packageObject.smartspeech.recognition.v1.RecognitionOptions.PCM_S16LE,
+  sample_rate: 16000,
+  language: 'ru-RU',
+  hypotheses_count: 1,
+  enable_profanity_filter: true,
+  enable_multi_utterance: false,
+  enable_partial_results: true,
+  model: 'general'
 };
 
 var jwtRequestOptions = {
@@ -55,14 +53,14 @@ var jwtRequest = https.get(jwtRequestOptions, function(jwtResponse) {
     var grpcCredentials = grpc.credentials.createSsl(fs.readFileSync('./roots.pem'));
     var service = new serviceConstructor('smartspeech.sber.ru:443', grpcCredentials);
     var call = service['Recognize'](serviceMetadata);
-    call.write(grpcRequestConfig);
+    call.write({options: recognitionOptions});
 
     let i = 1;
     const interval = setInterval(() => {
       if (i * CHUNK_SIZE <= audio.length) {
         const chunk = new Uint16Array(audio.slice((i - 1) * CHUNK_SIZE, i * CHUNK_SIZE));
         const chunkBuffer = Buffer.from(chunk);
-        call.write({audioContent: chunkBuffer});
+        call.write({audio_chunk: chunkBuffer});
         i++;
       } else {
         call.end();
